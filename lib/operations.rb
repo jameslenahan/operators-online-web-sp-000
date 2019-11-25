733c286cbb96dc2f392bcb7d23ea34ed5501af97
James Lenahan <james.k.lenahan@gmail.com>
2019-11-25 17:53:14 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -11,9 +11,9 @@ end
 
 
 def not_safe?(speed)
-	if speed > 60 ? true : false
-	elsif speed < 40 ? true : false
-	else speed 40 < 60 ? true : false
+	speed > 60 ? true : false
+	speed < 40 ? true : false
+	speed 40 < 60 ? true : false
 	end
 end
 	
